51a1795f5446d2f693fdc9d7e1a0e4cd4c1bd968
Liro Rodrigues <liro_rod.dev@hotmail.com>
2024-05-03 15:18:22 -0300

    Feat: Implemented block, blockchain and genesis

--- benechico-liroedas/src/models/blockchain.rs
+++ benechico-liroedas/src/models/blockchain.rs
@@ -3,7 +3,7 @@ use super::block::Block;
 
 type Blocks = Vec<Block>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Blockchain {
   pub genesis_block: Block,
   pub chain: Blocks,
@@ -13,7 +13,7 @@ pub struct Blockchain {
 impl Blockchain {
   pub fn new(difficulty: usize) -> Self {
     // genesis
-    let mut genesis_block = Block {
+    let genesis_block = Block {
       index: 0,
       timestamp: Utc::now().timestamp_millis() as u64,
       proof_of_work: u64::default(),
@@ -33,10 +33,9 @@ impl Blockchain {
   }
 
   pub fn add_block(&mut self, nonce: String) {
-    let new_block = Block::new(
+    let mut new_block = Block::new(
       self.chain.len() as u64,
       nonce,
-      self.chain[&self.chain.len() - 1].previous_hash.clone()
     );
     new_block.mine(self.clone());
     self.chain.push(new_block.clone());
